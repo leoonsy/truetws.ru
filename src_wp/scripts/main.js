86361ae4b3603db4ef7253cd06079cb7115490a0
leoonsy <yashanovsky.sergey@ya.ru>
2020-07-14 01:21:25 +0500

    add 2 section

--- src_wp/scripts/main.js
+++ src_wp/scripts/main.js
@@ -1,4 +1,10 @@
 import $ from 'jquery';
 import 'jquery.maskedinput/src/jquery.maskedinput';
+import WOW from 'wow.js/dist/wow.min';
 
 $(".header__mobile-number").mask("+7(999)999-99-99");
+$(() => {
+    new WOW({
+        animateClass: "animate__animated"
+    }).init();
+});
