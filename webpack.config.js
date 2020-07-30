4f62cc0f0a6b3f348c9fb282913ef6fc46203b16
leoonsy <yashanovsky.sergey@ya.ru>
2020-07-30 20:34:48 +0500

    add W3C validation

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 //конфигурация webpack
 const path = require('path')
 
-module.exports = (env, argv) => {
+//главная конфигурация для верстки
+const mainConfig = (env, argv) => {
     let mode = argv.mode || 'development';
     let isDevelopment = mode == 'development';
     let isProduction = !isDevelopment;
     console.log(mode);
 
-    let conf = {
+    const conf = {
+        name: 'main',
         context: path.resolve(__dirname, 'src_wp'),
         entry: {
             main: './scripts/main.js'
@@ -25,39 +27,8 @@ module.exports = (env, argv) => {
                     exclude: [/\/node_modules\//, /.min/],
                     loader: 'babel-loader'
                 },
-                // {
-                //     test: /\.css$/,
-                //     loader: 'css-loader'
-                // },
-                // {
-                //     test: /\.s[ac]ss$/,
-                //     loader: 'style-loader!css-loader!postcss-loader!sass-loader'
-                // },
-                // {
-                //     test: /\.(png|jpg|gif|svg)$/,
-                //     loader: 'file-loader',
-                //     options: {
-                //         name: `[path][name].[ext]`,
-                //         esModule: false,
-                //     }
-                // }
             ]
         },
-        // optimization: {
-        //     runtimeChunk: { name: 'common' },
-        //     splitChunks: {
-        //         cacheGroups: {
-        //             default: false,
-        //             commons: {
-        //                 test: /\.jsx?$/,
-        //                 chunks: 'all',
-        //                 minChunks: 2,
-        //                 name: 'common',
-        //                 enforce: true,
-        //             },
-        //         },
-        //     },
-        // },
         devServer: {
             contentBase: path.join(__dirname, 'dist'),
             watchContentBase: true
@@ -67,3 +38,5 @@ module.exports = (env, argv) => {
 
     return conf;
 };
+
+module.exports = [mainConfig];
