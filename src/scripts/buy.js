98a9deb3c911716d630ca923685a4578017290a3
leoonsy <yashanovsky.sergey@ya.ru>
2020-08-10 21:32:08 +0500

    add disabled state for submit buttons

--- src/scripts/buy.js
+++ src/scripts/buy.js
@@ -1,18 +1,37 @@
 import Order from './order';
 import MicroModal from "micromodal";
 
+function changeSubmitButtonsState(isAllowed = true) {
+    const submitButtons = document.querySelectorAll('input[type=submit]');
+    submitButtons.forEach(form => {
+            if (isAllowed) {
+                form.removeAttribute('disabled');
+                form.classList.remove('disabled');
+            }
+            else {
+                form.setAttribute('disabled', 'disabled');
+                form.classList.add('disabled');
+            }
+        }
+    );
+}
+
 const buyForms = [
     document.querySelector('.header__form'),
     document.querySelector('.modal-form'),
     document.querySelector('.buy-form__form'),
     document.querySelector('.end-form')
 ];
+
 const price = document.querySelector('.price__number-new').textContent.slice(0, -1);
 
 buyForms.forEach(form => {
-    form.addEventListener('submit',  async e => {
+    form.addEventListener('submit', async e => {
         e.preventDefault();
-        const data = { 
+
+        changeSubmitButtonsState(false);
+
+        const data = {
             name: form.name.value,
             phone: form.phone.value,
             hdn: form.hdn.value,
@@ -25,14 +44,13 @@ buyForms.forEach(form => {
         const modalContent = document.querySelector('#modal-message-content');
         try {
             const result = await order.sendOrder();
-            ym(66123454,'reachGoal','buy');
-            
+            ym(66123454, 'reachGoal', 'buy');
+
             modalTitle.innerHTML = "Спасибо за заказ!";
             modalTitle.classList.remove('text-danger');
-            
+
             modalContent.innerHTML = "Ваш заказ принят! В ближайшее время с Вами свяжется менеджер для уточнения заказа!";
-        }
-        catch (e) {
+        } catch (e) {
             let message;
             switch (e.type) {
                 case 'validationError':
@@ -41,17 +59,19 @@ buyForms.forEach(form => {
                 case 'mailError':
                     message = 'Ошибка при отправке письма. ' + e.message;
                     break;
-                default: 
+                default:
                     message = 'Произошла неизвестная ошибка';
                     break;
             }
 
             modalTitle.innerHTML = "Ошибка!";
             modalTitle.classList.add('text-danger');
-            
+
             modalContent.innerHTML = message;
         }
-        
+
+        changeSubmitButtonsState(true);
+
         MicroModal.show('modal-message', {
                 onClose: () => {
                     if (form.className === 'modal-form')
